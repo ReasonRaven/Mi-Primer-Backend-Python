0c4566627e9528bc290b08c70d7301db6cf5c873
ReasonRaven <jonathan23_09@outlook.com>
2024-11-06 13:36:08 -0600

    arreglo de backend con todas las paginas

--- app.py
+++ app.py
@@ -6,6 +6,24 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/misProyectos')
+def misProyectos():
+    return render_template('misProyectos.html')
+@app.route('/pagina')
+def pagina():
+    return render_template('mi_Pagina_WEB.html')
+@app.route('/koala')
+def koala():
+    return render_template('scanerKoala.html')
+@app.route('/reporteVR')
+def reporte():
+    return render_template('reporte.html')
+@app.route('/frameworks')
+def frameworks():
+    return render_template('frameworks.html')
+@app.route('/stackoverflow')
+def stackoverflow():
+    return render_template('stackOverflow.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
