b417521d004ec5e1d9f0445b0bb4ac172e05303c
ReasonRaven <jonathan23_09@outlook.com>
2024-11-12 19:22:25 -0600

    add upgrade gunicorn

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 
 app = Flask(__name__)
+
+usuario = {
+    'email': 'jonathan23_09@outlook.com',
+    'password': 'batman2006'
+}
+
 
 @app.route('/')
 def home():
@@ -24,6 +30,16 @@ def frameworks():
 @app.route('/stackoverflow')
 def stackoverflow():
     return render_template('stackOverflow.html')
-
+@app.route('/success', methods=['POST'])
+def success():
+    email = request.form['email']
+    password = request.form['password']
+    print(email,password)
+    if email == usuario['email'] and password == usuario['password']:
+        return render_template('success.html')
+    else:
+        print('Vete')
+        return redirect(url_for('home'))
+    
 if __name__ == '__main__':
     app.run(debug=True)
